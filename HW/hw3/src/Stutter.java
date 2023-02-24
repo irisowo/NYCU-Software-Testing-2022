e4e2dbc2d826d40d3faaa20e46ba4585175b3410
IrisChiu <iris0106.cs06@nctu.edu.tw>
2023-02-24 15:26:46 +0800

    fix the src code in hw3

--- HW/hw3/src/Stutter.java
+++ HW/hw3/src/Stutter.java
@@ -1,125 +1,107 @@
-// Authors: Paul Ammann & Jeff Offutt
-// Chapter 7; page ??
-// Can be run from command line
-// See Stutter.num for a numbered version.
-// No JUnit tests at this time.
-
-/** *****************************************************
- // Stutter checks for repeat words in a text file.
- // It prints a list of repeat words, by line number.
- // Stutter will accept standard input or a list
- // of file names.
- *  Jeff Offutt, June 1989 (in C), Java version March 2003
- ********************************************************* */
-import java.io.*;
-
-public class Stutter
-{
-    // Class variables used in multiple methods.
-    private static boolean lastdelimit = true;
-    private static String curWord = "", prevWord = "";
-    private static char delimits [] =
-            {'	', ' ', ',', '.', '!', '-', '+', '=', ';', ':', '?',
-                    '&', '{', '}', '\\'}; // First char in list is a tab
-
-    //************************************************
-    // main parses the arguments, decides if stdin
-    // or a file name, and calls Stut().
-    //************************************************
-    public static void main (String[] args) throws IOException
-    {
-        String fileName;
-        FileReader myFile;
-        BufferedReader inFile = null;
-
-        if (args.length == 0)
-        {  // no file, use stdin
-            inFile = new BufferedReader (new InputStreamReader (System.in));
-        }
-        else
-        {
-            fileName = args [0];
-            if (fileName == null)
-            {  // no file name, use stdin
-                inFile = new BufferedReader (new InputStreamReader (System.in));
-            }
-            else
-            {  // file name, open the file.
-                myFile = new FileReader (fileName);
-                inFile = new BufferedReader (myFile);
-            }
-        }
-
-        stut (inFile);
-    }
-
-    //************************************************
-    // Stut() reads all lines in the input stream, and
-    // finds words. Words are defined as being surrounded
-    // by delimiters as defined in the delimits[] array.
-    // Every time an end of word is found, checkDupes()
-    // is called to see if it is the same as the
-    // previous word.
-    //************************************************
-    private static void stut (BufferedReader inFile) throws IOException
-    {
-        String inLine;
-        char c;
-        int linecnt = 1;
-        while ((inLine = inFile.readLine()) != null)
-        {  // For each line
-
-            for (int i=0; i<inLine.length(); i++)
-            {  // for each character
-                c = inLine.charAt(i);
-
-                if (isDelimit (c))
-                {  // Found an end of a word.
-                    checkDupes (linecnt);
-                }
-                else
-                {
-                    lastdelimit = false;
-                    curWord = curWord + c;
-                }
-            }
-            checkDupes (linecnt);
-            linecnt++;
-
-        }
-    }  // end Stut
-
-    //************************************************
-    // checkDupes() checks to see if the globally defined
-    // curWord is the same as prevWord and prints a message
-    // if they are the same.
-    //************************************************
-    private static void checkDupes (int line)
-    {
-        if (lastdelimit)
-            return; // already checked, keep skipping
-
-        lastdelimit = true;
-        if (curWord.equals(prevWord))
-        {
-            System.out.println ("Repeated word on line " + line + ": " +
-                    prevWord+ " " + curWord);
-        }
-        else
-        {
-            prevWord = curWord;
-        }
-        curWord = "";
-    }  // end checkDupes
-
-    //************************************************
-    // Checks to see if a character is a delimiter.
-    //************************************************
-    private static boolean isDelimit (char C)
-    {
-        for (int i = 0; i < delimits.length; i++)
-            if (C == delimits [i])
-                return (true);
-        return (false);
-    }
-} // end class Stutter
+     1	// Jeff Offutt - Junie 1989, Java version 2003
+     2	
+     3	// Stutter checks for repeat words in a text file.
+     4	// It prints a list of repeat words, by line number.
+     5	// Stutter will accept standard input or a list
+     6	// of file names.
+     7	
+     8	import java.io.*;
+     9	
+    10	class Stutter
+    11	{
+    12	  // Class variables used in multiple methods.
+    13	  private static boolean lastdelimit = true;
+    14	  private static String curWord = "", prevWord = "";
+    15	  private static char delimits [] =
+    16	          {'	', ' ', ',', '.', '!', '-', '+', '=', ';', ':', '?', '&', '{', '}', '\\'};
+    17	
+    18	//************************************************
+    19	// main parses the arguments, decides if stdin
+    20	// or a file name, and calls Stut().
+    21	//************************************************
+    22	public static void main (String[] args) throws IOException
+    23	{
+    24	   String fileName;
+    25	   FileReader myFile;
+    26	   BufferedReader inFile = null;
+    27	
+    28	   if (args.length == 0)
+    29	   {  // no file, use stdin
+    30	      inFile = new BufferedReader (new InputStreamReader (System.in));
+    31	   }
+    32	   else
+    33	   {
+    34	      fileName = args [0];
+    35	      if (fileName == null)
+    36	      {  // no file name, use stdin
+    37	         inFile = new BufferedReader (new InputStreamReader (System.in));
+    38	      }
+    39	      else
+    40	      {  // file name, open the file.
+    41	         myFile = new FileReader (fileName);
+    42	         inFile = new BufferedReader (myFile);
+    43	      }
+    44	   }
+    45	
+    46	   stut (inFile);
+    47	}
+    48	
+    49	//************************************************
+    50	//************************************************
+    51	private static void stut (BufferedReader inFile) throws IOException
+    52	{
+    53	   String inLine;
+    54	   char c;
+    55	   int linecnt = 1;
+    56	   while ((inLine = inFile.readLine()) != null)
+    57	   {  // For each line
+    58	
+    59	      for (int i=0; i<inLine.length(); i++)
+    60	      {  // for each character
+    61	         c = inLine.charAt(i);
+    62	
+    63	         if (isDelimit (c))
+    64	         {  // Found an end of a word.
+    65	            checkDupes (linecnt);
+    66	         }
+    67	         else
+    68	         {
+    69	           lastdelimit = false;
+    70	           curWord = curWord + c;
+    71	         }
+    72	      }
+    73	      checkDupes (linecnt);
+    74	      linecnt++;
+    75	
+    76	   }
+    77	}  // end Stut
+    78	
+    79	//************************************************
+    80	//************************************************
+    81	private static void checkDupes (int line)
+    82	{
+    83	   if (lastdelimit)
+    84	   return; // already checked, keep skipping
+    85	   lastdelimit = true;
+    86	   if (curWord.equals(prevWord))
+    87	   {
+    88	      System.out.println ("Repeated word on line " + line + ": " + prevWord+ " " + curWord);
+    89	   }
+    90	   else
+    91	   {
+    92	      prevWord = curWord;
+    93	   }
+    94	   curWord = "";
+    95	}  // end checkDupes
+    96	
+    97	//************************************************
+    98	//************************************************
+    99	private static boolean isDelimit (char C)
+   100	{
+   101	   for (int i = 0; i < delimits.length; i++)
+   102	      if (C == delimits [i])
+   103	         return (true);
+   104	   return (false);
+   105	}
+   106	
+   107	} // end class Stutter
